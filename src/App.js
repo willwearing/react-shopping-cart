9d2b1cadbebaed6d52b2b324ee59052f385e0beb
willwearing <willwearing@gmail.com>
2020-10-08 13:48:29 -0600

    mvp and stretch done

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, { createContext, useState } from "react";
+import { Route } from "react-router-dom";
+import data from "./data";
 
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
+
+export const Shopping = createContext();
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		// add the given item to the cart
-	};
+  const addItem = (item) => {
+    setCart([...cart, item]);
+  };
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+  const removeItem = (item) => {
+    const newCart = cart.filter((product, index) => index !== item);
+    setCart(newCart);
+  };
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+  return (
+    <div className="App">
+      <Shopping.Provider value={{ products, cart, addItem, removeItem }}>
+        <Navigation />
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
-	);
+        {/* Routes */}
+        <Route exact path="/">
+          <Products />
+        </Route>
+
+        <Route path="/cart">
+          <ShoppingCart />
+        </Route>
+      </Shopping.Provider>
+    </div>
+  );
 }
 
 export default App;
